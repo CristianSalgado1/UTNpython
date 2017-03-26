4a3f05cc02532a4e344930e38d8ed339a448066c
lucariel <lucariel92@gmail.com>
2017-03-26 13:30:10 -0300

    first commit

---
+++ boton1.py
@@ -0,0 +1,20 @@
+from tkinter import *
+
+
+class HolaButton():
+    def __init__(self,parent = None, **config):
+
+        self.myParent = parent
+        self.myParent.geometry("300x300")
+        button = Button(self.myParent, **config)
+        button.pack()
+        button.config(command = self.callback)
+
+    def callback(self):
+        print("Adios...")
+        self.myParent.quit()
+
+if __name__ == "__main__":
+    root = Tk()
+    miAplicacion = HolaButton(root, text = "Hello Subclass world")
+    root.mainloop()
