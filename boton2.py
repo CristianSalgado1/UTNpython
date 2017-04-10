52f40b8f80e53304b57ceb0ff740c863279b568b
lucariel <lucariel92@gmail.com>
2017-04-10 06:59:37 -0300

    boton2

--- boton2.py
+++ boton2.py
@@ -1,12 +1,2 @@
-from tkinter import *
-from boton1 import HolaButton
-
-class MiButton(HolaButton):
-    def callback(self):
-        print("Esto es polimorfismo!")
-
-if __name__ == "__main__":
-    root = Tk()
-    root.geometry("300x200")
-    MiButton(root, text = "Boton de subclase")
-    root.mainloop()
+for a in range(10):
+    print(a)
